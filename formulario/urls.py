9bd2aaabafaf40b3d0079a9968861ffd4a3da26c
Miguel Angel Alvarez <maagfis@gmail.com>
2022-06-08 17:39:24 -0500

    v 0.16.18.16
    Se implementa la lógica para avanzar en los registros y proporcionar el último registrado de los que están para remarcar

--- formulario/urls.py
+++ formulario/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('formulario/', views.formulario, name='formulario'),
-    path('submit_registro/<str:cedis>/<str:pais>/<str:campania>/<int:num_dist>', views.submit_registro, name='submit_registro'),
-    path('formulario2/<str:pais>/<str:cedis>/<str:campania>', views.formulario2, name='formulario2'),
+    path('submit_registro/<str:cedis>/<str:pais>/<str:campania>/<int:num_dist>/', views.submit_registro, name='submit_registro'),
+    path('formulario2/<str:pais>/<str:cedis>/<str:campania>/', views.formulario2, name='formulario2'),
 ]
 
