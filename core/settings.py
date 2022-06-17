c5d1fd7da3aafcf5a87ca8a630c213adc915615a
Miguel Angel Alvarez <maagfis@gmail.com>
2022-06-17 14:48:34 -0500

    update settings.py
    update environ

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import environ # para las variables de entorno
 
 env = environ.Env()
-env.read_env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = env.bool('DEBUG', default=False)
 
 # se puso el host al cambiar DEBUG a False
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
