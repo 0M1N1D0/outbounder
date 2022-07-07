9a71eeed481acf09b140a9b46f831edb9f0225d3
Miguel Angel Alvarez <maagfis@gmail.com>
2022-07-07 14:54:07 -0500

    from 404
    Se agrega bootstrap a template error404

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = env.bool('DEBUG', default=False)
 DEBUG = False
+# DEBUG404 = True
 
 # se puso el host al cambiar DEBUG a False
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
