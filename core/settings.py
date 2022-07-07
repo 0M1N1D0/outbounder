1622d02df19a2bc1769f5c457720740388d01339
Miguel Angel Alvarez <maagfis@gmail.com>
2022-07-07 14:31:34 -0500

    logo error 404

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=False)
+# DEBUG = env.bool('DEBUG', default=False)
+DEBUG = False
 
 # se puso el host al cambiar DEBUG a False
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
