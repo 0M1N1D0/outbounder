9c1cc522574fc1c9d76e7e9fb719fad199c0b1d1
Miguel Angel Alvarez <maagfis@gmail.com>
2022-07-13 16:19:38 -0500

    front formulario2-submit
    Se termina el front de los templates formulario 2 y submit registro

--- formulario/static/formulario/js/main.js
+++ formulario/static/formulario/js/main.js
@@ -5,23 +5,25 @@
 // let regis_exitoso = document.getElementById('regis_exitoso');
 // let regis_no_exitoso = document.getElementById('regis_no_exitoso');
 
-document.getElementById('regis_no_exitoso').disabled = true;
+document.getElementById('regis_no_exitoso').style.visibility = "hidden"; //= true;
 
 //check_btn_1.style.backgroundColor ="#0d6efd";
-
+console.log('Encendido!');
 function chequeo(checkbox){
     if(checkbox.checked){
-        // console.log('Encendido!');
-        document.getElementById('regis_no_exitoso').disabled = false;
+        
+        document.getElementById('regis_no_exitoso').style.visibility="visible";// disabled = false;
         // limpia el valor seleccionado antes de inhabilitarlo
         document.getElementById("regis_exitoso").options.item(0).selected = 'selected';
-        document.getElementById('regis_exitoso').disabled = true;
+        document.getElementById('regis_exitoso').style.visibility = "hidden";
+        
+        
     }
     else{
         // console.log('Apagado!');
         // limpia el valor seleccionado antes de inhabilitarlo
         document.getElementById("regis_no_exitoso").options.item(0).selected = 'selected';
-        document.getElementById('regis_no_exitoso').disabled = true;
-        document.getElementById('regis_exitoso').disabled = false;
+        document.getElementById('regis_no_exitoso').style.visibility = "hidden";
+        document.getElementById('regis_exitoso').style.visibility="visible";
     }
 }
