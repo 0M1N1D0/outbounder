4f3efc8098fff6b161279ca2a5e94bdcd2dd9d3b
Miguel Angel Alvarez <maagfis@gmail.com>
2022-07-22 11:10:38 -0500

    .

--- formulario/urls.py
+++ formulario/urls.py
@@ -1,13 +1,15 @@
-
 from django.urls import path
 from . import views
 
 # app_name = 'formulario'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('formulario/', views.formulario, name='formulario'),
-    # TODO: cambiar int por str
-    path('submit_registro/<str:cedis>/<str:pais>/<str:campania>/<int:num_dist>/', views.submit_registro, name='submit_registro'),
-    path('formulario2/<str:pais>/<str:cedis>/<str:campania>/<int:registros_totales>/<int:total_no_exitosos>/<int:total_exitosos>/', views.formulario2, name='formulario2'),
+	path('', views.index, name='index'),
+	path('formulario/', views.formulario, name='formulario'),
+	# TODO: cambiar int por str
+	path('submit_registro/<str:cedis>/<str:pais>/<str:campania>/<int:num_dist>/', views.submit_registro,
+	     name='submit_registro'),
+	path(
+		'formulario2/<str:pais>/<str:cedis>/<str:campania>/<int:registros_totales>/<int:total_no_exitosos>/<int'
+		':total_exitosos>/',
+		views.formulario2, name='formulario2'),
 ]
-
